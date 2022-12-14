ac071af6a727e005717fb6d52b934a8c9211a6d7
Rendra Piscela <rendraprojects@gmail.com>
2022-12-14 11:22:19 +0700

    Latest update feat backward

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import * as Device from 'expo-device';
+import * as TaskManager from 'expo-task-manager';
 import * as Notifications from 'expo-notifications';
-import {NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import React, { useState, useEffect, useRef, useCallback } from 'react';
-import { StyleSheet, RefreshControl, BackHandler, ScrollView, SafeAreaView, StatusBar, Dimensions } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, BackHandler, SafeAreaView, PermissionsAndroid } from 'react-native';
 import { WebView } from 'react-native-webview';
+// import RNSimData from "react-native-sim-data";
+
+PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE)
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
-    shouldPlaySound: true,
-    shouldSetBadge: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
   }),
 });
 
@@ -36,28 +40,42 @@ export async function requestPermissionsAsync() {
   });
 }
 
+const BACKGROUND_NOTIFICATION_TASK = 'BACKGROUND-NOTIFICATION-TASK';
+
+TaskManager.defineTask(BACKGROUND_NOTIFICATION_TASK, ({ data, error, executionInfo }) => {
+  // console.log('Received a notification in the background!');
+  // Do something with the notification data
+});
+
+Notifications.registerTaskAsync(BACKGROUND_NOTIFICATION_TASK);
+
 export default function App() {
+
+  const lastNotificationResponse = Notifications.useLastNotificationResponse();
   const [expoPushToken, setExpoPushToken] = useState('');
   const [notification, setNotification] = useState(false);
   const [page, setPage] = useState('');
   const notificationListener = useRef();
-  const responseListener = useRef();  
-  const domain = 'https://intra21.emp.id/';
-  const idUser = `
+  const responseListener = useRef();
+  const domain = 'https://intra21.emp.id/login';
+  // const domain = 'http://5518-111-95-220-21.ngrok.io/login'; //for testing
+  const [currentUrl, setCurrentUrl] = useState(domain+'?extend=https://intra21.emp.id');
+  // const [currentUrl, setCurrentUrl] = useState(domain+'?extend=http://e057-139-192-108-91.ngrok.io');
+  var idUser = `
   document.getElementById('expotoken').value = '${expoPushToken}';
+  document.getElementById('extend').value = '${currentUrl}';
   document.querySelector('.dt-button').style.display = 'none';
   window.ReactNativeWebView.postMessage(Math.max(document.body.offsetHeight, document.body.scrollHeight));
   `;
-  const [baseUrl] = useState(domain);
-  
+  PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE)
+  PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_PHONE_NUMBERS)
   const WebViewPage = ({navigation}) => {
+
     const WEBVIEW_REF = useRef(null);
     const [canGoBack, setCanGoBack] = useState(false);
     const [canGoForward, setCanGoForward] = useState(false);
-    const [currentUrl, setCurrentUrl] = useState(domain);
-    
+
     const backAction = () => {
-      // console.log(canGoBack)
       if(canGoBack){
         WEBVIEW_REF.current.goBack();
       }else{
@@ -65,83 +83,77 @@ export default function App() {
       }
       return true;
     };
+    const forwardAction = () => {
+      // console.log("forward : "+canGoBack)
+      if(canGoForward){
+        WEBVIEW_REF.current.goForward();
+      }
+      return true;
+    };
 
-    useEffect(() => {
-    
-    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));  
+    useEffect(() => {    
 
-        BackHandler.addEventListener('hardwareBackPress', backAction);
-              // This listener is fired whenever a notification is received while the app is foregrounded
+        registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+        // This listener is fired whenever a notification is received while the app is foregrounded. app is open
         notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
           setNotification(notification);
           const onInteraction = notification.request.content.data.event;
             setCurrentUrl(onInteraction);
         });
 
-        // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+        // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed) app is closed
         responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-          // console.log(response);
           const onInteraction = response.notification.request.content.data.event;
           setCurrentUrl(onInteraction);
-          // console.log(onInteraction+'response')
         });
-    return () => {
-      BackHandler.removeEventListener('hardwareBackPress', backAction);
-      Notifications.removeNotificationSubscription(notificationListener.current);
-      Notifications.removeNotificationSubscription(responseListener.current);
-    };
-    },[canGoBack]);
 
-    //create function reload webview, scroll bottom
-    const [refreshing, setRefreshing] = React.useState(false);
-    const onRefresh = React.useCallback(() => {
-      setRefreshing(true);
-      WEBVIEW_REF.current.reload();
-      setRefreshing(false);
-    }, [refreshing]);
+        Notifications.registerTaskAsync(BACKGROUND_NOTIFICATION_TASK)
+        BackHandler.addEventListener('hardwareBackPress', backAction);
+
+        return () => {
+          BackHandler.removeEventListener('hardwareBackPress', backAction);
+          Notifications.removeNotificationSubscription(notificationListener.current);
+          Notifications.removeNotificationSubscription(responseListener.current);
+          Notifications.unregisterTaskAsync(BACKGROUND_NOTIFICATION_TASK)
+        };
+      },[canGoBack, lastNotificationResponse]); //end use effect
+
+          
     const [webViewHeight, setWebViewHeight] = React.useState(null);
   
     const onMessage = (event) => {
       setWebViewHeight(Number(event.nativeEvent.data));
     }
-
     return (
-        // <ScrollView contentContainerStyle={{ flexGrow: 1, height: webViewHeight }}
-        // refreshControl={
-        //   <RefreshControl
-        //     refreshing={refreshing}
-        //     onRefresh={onRefresh}
-        //   />
-        // }>
-     <WebView
-          // javaScriptEnabledAndroid={true}
-          originWhitelist={['*']}
-          allowFileAccess={true}
-          allowFileAccessFromFileURLs={true}
-          style={styles.webViewStyle}
-          javaScriptCanOpenWindowsAutomatically={true}
-          source={{ uri: `${currentUrl}`, "Accept-Language": navigator.language,
-          "User-Agent": Platform.OS, }}
-          javaScriptEnabled={true}
-          domStorageEnabled={true}
-          onMessage={onMessage}
-          injectedJavaScript={idUser}
-          allowsBackForwardNavigationGestures={true}
-          requestDisallowInterceptTouchEvent={true}
-          downloadingMessage={'Downloading...'}
-          ref={WEBVIEW_REF}
-          onNavigationStateChange={(navState) => {
-            setCanGoBack(navState.canGoBack);
-            setCanGoForward(navState.canGoForward);
-            setCurrentUrl(navState.url);
-          }}
-      />
-      // </ScrollView>
+      <SafeAreaView style={{ flex: 1 }}>
+        <WebView
+              javaScriptEnabledAndroid={true}
+              originWhitelist={['*']}
+              allowFileAccess={true}
+              allowFileAccessFromFileURLs={true}
+              style={styles.webViewStyle}
+              javaScriptCanOpenWindowsAutomatically={true}
+              source={{ uri: `${currentUrl}`, "Accept-Language": navigator.language }}
+              javaScriptEnabled={true}
+              domStorageEnabled={true}
+              nestedScrollEnabled={true}
+              onMessage={onMessage}
+              injectedJavaScript={idUser}
+              allowsBackForwardNavigationGestures={true}
+              requestDisallowInterceptTouchEvent={true}
+              downloadingMessage={'Downloading...'}
+              ref={WEBVIEW_REF}
+              onNavigationStateChange={(navState) => {               
+                setCanGoBack(navState.canGoBack);
+                // setCurrentUrl(navState.url);
+                // setCanGoForward(navState.canGoForward);
+              }}
+          />
+      </SafeAreaView>
     );
   };
   //create stack navigator
   const Stack = createStackNavigator();
-
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -166,7 +178,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 1,
     position: 'relative',
-    marginTop: 40,
+    // marginTop: 35,
   },
   activityIndicatorStyle: {
     flex: 1,
@@ -178,27 +190,6 @@ const styles = StyleSheet.create({
     backgroundColor : 'white'
    }
 });
-
-// Can use this function below, OR use Expo's Push Notification Tool-> https://expo.dev/notifications
-async function sendPushNotification(expoPushToken) {
-  const message = {
-    to: expoPushToken,
-    sound: 'default',
-    title: 'Original Title',
-    body: 'And here is the body!',
-    data: { someData: 'goes here' },
-  };
-
-  await fetch('https://exp.host/--/api/v2/push/send', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Accept-encoding': 'gzip, deflate',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(message),
-  });
-}
 
 async function registerForPushNotificationsAsync() {
   let token;
@@ -227,6 +218,5 @@ async function registerForPushNotificationsAsync() {
       sound: 'default',
     });
   }
-  // console.log(token)
   return token;
 }
